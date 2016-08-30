abbe40c30e359686e33c5e8d96c1f7efa275239b
SasNaw <SaschaNawrot@gmail.com>
2016-08-30 20:38:14 +0200

    can be executed on non-dzis, added parameter -o, valid input is folder or file

--- TessellationService.py
+++ TessellationService.py
@@ -1,42 +1,168 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# TODO delete this:
+# /home/sawn/Studium/Master/microservices/TessellationService/TessellationService.py
+#
+
+from openslide import OpenSlide
+from PIL import Image
+import json
+import sys
+import numpy as np
+import os, os.path
+import argparse
+
+
+PATH = ''
+TILE_SIZE = 256
+global OUTPUT
+OUTPUT = None
+
+
+def read_json(path):
+    try:
+        with open(path, 'r') as file:
+            return json.loads(file.read())
+    except IOError:
+        print('Could not load saved annotations from ' + path)
+
+
+def get_bounding_box(region):
+    x_min = sys.float_info.max
+    x_max = sys.float_info.min
+    y_min = x_min
+    y_max = x_max
+    for coordinate in region.get('imgCoords'):
+        x = coordinate.get('x')
+        y = coordinate.get('y')
+        if(x >= x_max):
+            x_max = x
+        elif(x < x_min) :
+            x_min = x
+        if(y >= y_max):
+            y_max = y
+        elif(y < y_min) :
+            y_min = y
+
+    return {'x_max': int(np.ceil(x_max)), 'x_min': int(np.floor(x_min)),
+            'y_max': int(np.ceil(y_max)), 'y_min': int(np.floor(y_min))}
+
+
+def stitch_image(bounding_box, slide):
+
+    # img_width = int(np.ceil((bounding_box['x_max'] - bounding_box['x_min']) / TILE_SIZE) * TILE_SIZE)
+    # img_height = int(np.ceil((bounding_box['y_max'] - bounding_box['y_min']) / TILE_SIZE) * TILE_SIZE)
+    # img_stitched = Image.new('RGB', (img_width, img_height), color='#ff0000')
+    #
+    # offset_x = int(np.ceil(bounding_box['x_min'] / TILE_SIZE) * TILE_SIZE)
+    # offset_y = int(np.ceil(bounding_box['y_min'] / TILE_SIZE) * TILE_SIZE)
+    #
+    # for x in range(0, (img_width / 256)):
+    #     for y in range(0, (img_height/ 256)):
+    #
+    #         tile = slide.read_region((x * TILE_SIZE + offset_x, y * TILE_SIZE + offset_y), 0, (TILE_SIZE, TILE_SIZE))
+    #         img_stitched.paste(tile, (x*TILE_SIZE, y*TILE_SIZE))
+
+    return img_stitched
+
+
+def extract_image_from_region_dzi(slide, region):
+    width, height = slide.dimensions
+    return ''
+
+
+def extract_image_from_region_wsi(slide, region):
+    bounding_box = get_bounding_box(region)
+    location = (bounding_box['x_min'], bounding_box['y_min'])
+    size = (bounding_box['x_max'] - bounding_box['x_min'], bounding_box['y_max'] - bounding_box['y_min'])
+    return slide.read_region(location, 0, size)
+
+
+def extract_image_from_region(slide, slide_name, region, dzi):
+    if(dzi):
+        image = extract_image_from_region_dzi(slide, region)
+    else:
+        image = extract_image_from_region_wsi(slide, region)
+    if(OUTPUT):
+        dest = OUTPUT + region['name']
+    else:
+        dest = region['name']
+    if not os.path.exists(dest):
+        os.makedirs(dest)
+    image.save(dest + '/' + slide_name + '_' + str(region['uid']), 'jpeg')
+
+
+def extract_regions(file):
+    slide = OpenSlide(file)
+    slide_name = file.split('/')[-1]
+    regions = read_json(file + '.json')
+    for region in regions:
+        extract_image_from_region(slide, slide_name, region, 0)
+    slide.close()
+
+
+def is_suppoted(file):
+    ext = (file.split('.'))[-1]
+    if(
+                    'bif' in ext or
+                    'mrxs' in ext or
+                    'npdi' in ext or
+                    'scn' in ext or
+                    'svs' in ext or
+                    'svslide' in ext or
+                    'tif' in ext or
+                    'tiff' in ext or
+                    'vms' in ext or
+                    'vmu' in ext
+       ):
+        return 1
+    else:
+        return 0
+
+
+def extract_regions_from_dir(dir):
+    if not dir.endswith('/'):
+        dir = dir + '/'
+    contents = os.listdir(dir)
+    for content in contents:
+        if os.path.isdir(dir + content):
+            extract_regions_from_dir(dir + content)
+        else:
+            if(is_suppoted(content)):
+                extract_regions(dir + content)
+
+
+def run(input):
+    if(os.path.isfile(input)):
+        extract_regions(input)
+    elif(os.path.isdir(input)):
+        extract_regions_from_dir(input)
+    # TODO: list
+    else:
+        print("ERROR: " + input + " could not be found!")
+
 
 if __name__ == '__main__':
-    # parser = OptionParser(usage='Usage: %prog [options] [slide]')
-    # parser.add_option('-B', '--ignore-bounds', dest='DEEPZOOM_LIMIT_BOUNDS',
-    #                   default=True, action='store_false',
-    #                   help='display entire scan area')
-    # parser.add_option('-c', '--config', metavar='FILE', dest='config',
-    #                   help='config file')
-    # parser.add_option('-d', '--debug', dest='DEBUG', action='store_true',
-    #                   help='run in debugging mode (insecure)')
-    # parser.add_option('-e', '--overlap', metavar='PIXELS',
-    #                   dest='DEEPZOOM_OVERLAP', type='int',
-    #                   help='overlap of adjacent tiles [1]')
-    # parser.add_option('-f', '--format', metavar='{jpeg|png}',
-    #                   dest='DEEPZOOM_FORMAT',
-    #                   help='image format for tiles [jpeg]')
-    # parser.add_option('-l', '--listen', metavar='ADDRESS', dest='host',
-    #                   default='127.0.0.1',
-    #                   help='address to listen on [127.0.0.1]')
-    # parser.add_option('-p', '--port', metavar='PORT', dest='port',
-    #                   type='int', default=5000,
-    #                   help='port to listen on [5000]')
-    # parser.add_option('-Q', '--quality', metavar='QUALITY',
-    #                   dest='DEEPZOOM_TILE_QUALITY', type='int',
-    #                   help='JPEG compression quality [75]')
-    # parser.add_option('-s', '--size', metavar='PIXELS',
-    #                   dest='DEEPZOOM_TILE_SIZE', type='int',
-    #                   help='tile size [256]')
-    #
-    # (opts, args) = parser.parse_args()
-    # # Load config file if specified
-    # if opts.config is not None:
-    #     app.config.from_pyfile(opts.config)
-    # # Overwrite only those settings specified on the command line
-    # for k in dir(opts):
-    #     if not k.startswith('_') and getattr(opts, k) is None:
-    #         delattr(opts, k)
-    # app.config.from_object(opts)
-    #
-    # app.run(host=opts.host, port=opts.port, threaded=True)
-    print("test")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input", help="Either a single file, a list of files or a folder")
+    parser.add_argument("-o", "--output", help="directory to store extracted images")
+    parser.add_argument("-r", "--resize", help="resize all extracted images to provided value", type=int)
+    parser.add_argument("-t", "--tessellate", help="blub", type=int)
+
+
+    args = parser.parse_args()
+
+    if(args.resize and args.tessellate):
+        print("Only one of -r, -t can be chosen at the same time")
+    else:
+        if(args.output):
+            OUTPUT = args.output
+            if not OUTPUT.endswith('/'):
+                OUTPUT = OUTPUT + '/'
+            if not os.path.exists(OUTPUT):
+                os.makedirs(OUTPUT)
+
+        run(args.input)
+
+

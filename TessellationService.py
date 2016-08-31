f035a6ed7b14e0ec237b0998e8bca1614998a064
SasNaw <SaschaNawrot@gmail.com>
2016-08-31 19:23:26 +0200

    added -r

--- TessellationService.py
+++ TessellationService.py
@@ -22,6 +22,8 @@ global FORCE
 FORCE = False
 global RESIZE
 RESIZE = None
+RESIZE_WIDTH = 0
+RESIZE_HEIGHT = 1
 
 
 # ======================================   UTILITY   ======================================
@@ -47,8 +49,8 @@ def save_image(image, region, slide_name):
     if not FORCE:
         while os.path.isfile(name):
             name = name + "_copy"
-    if not RESIZE is None:
-        print("resize me! :D")
+    if RESIZE:
+        image = image.resize(RESIZE)
     image.save(name, 'jpeg')
     with open(name + '.context', 'w+') as file:
         data = json.dumps(region.get('context'), ensure_ascii=False)
@@ -100,6 +102,79 @@ def get_bounding_box(region):
             'y_max': int(np.ceil(y_max)), 'y_min': int(np.floor(y_min))}
 
 
+def resize_bounding_box(bounding_box):
+    r_ratio = RESIZE[RESIZE_WIDTH] / float(RESIZE[RESIZE_HEIGHT])
+    bb_width = float(bounding_box['x_max'] - bounding_box['x_min'])
+    bb_height = float(bounding_box['y_max'] - bounding_box['y_min'])
+    bb_ratio = bb_width / bb_height
+    if r_ratio == bb_ratio:
+        return bounding_box
+    else:
+        if r_ratio == 1:
+            # target is square
+            s1 = bb_height/bb_width
+            s2 = bb_width/bb_height
+            scaled = min(bb_width, bb_height) * max(s1, s2) - min(bb_width, bb_height)
+            if(bb_width > bb_height):
+                bounding_box['y_min'] -= int(np.floor(scaled/2))
+                bounding_box['y_max'] += int(np.ceil(scaled/2))
+            else:
+                bounding_box['x_min'] -= int(np.floor(scaled/2))
+                bounding_box['x_max'] += int(np.ceil(scaled/2))
+        elif r_ratio < 1:
+            # target is higher than wide
+            h_s = 1 / r_ratio
+            if bb_height > (bb_width * h_s):
+                # adjust width:
+                w_new = (bb_height / h_s) - bb_width
+                bounding_box['x_min'] -= int(np.floor(w_new/2))
+                bounding_box['x_max'] += int(np.ceil(w_new/2))
+            else:
+                # adjust height:
+                h_new = h_s * bb_width - bb_height
+                bounding_box['y_min'] -= int(np.floor(h_new/2))
+                bounding_box['y_max'] += int(np.ceil(h_new/2))
+        else:
+            # target is wider than high
+            w_s = r_ratio
+            if bb_width > (bb_height * w_s):
+                # adjust height
+                h_new = (bb_width / w_s) - bb_height
+                bounding_box['y_min'] -= int(np.floor(h_new/2))
+                bounding_box['y_max'] += int(np.ceil(h_new/2))
+            else:
+                # adjust width:
+                w_new = w_s * bb_height - bb_width
+                bounding_box['x_min'] -= int(np.floor(w_new/2))
+                bounding_box['x_max'] += int(np.ceil(w_new/2))
+
+        # check if bb is big enough
+        bb_width = float(bounding_box['x_max'] - bounding_box['x_min'])
+        if bb_width < RESIZE[RESIZE_WIDTH]:
+            s = RESIZE[RESIZE_WIDTH] / bb_width
+            bounding_box = scale_bounding_box(bounding_box, s)
+        bb_height = float(bounding_box['y_max'] - bounding_box['y_min'])
+        if bb_height < RESIZE[RESIZE_HEIGHT]:
+            s = RESIZE[RESIZE_HEIGHT] / bb_height
+            bounding_box = scale_bounding_box(bounding_box, s)
+
+        return bounding_box
+
+
+def scale_bounding_box(bounding_box, scale):
+    bb_width = float(bounding_box['x_max'] - bounding_box['x_min'])
+    add_w = (bb_width * scale) - bb_width
+    bounding_box['x_min'] -= int(np.floor(add_w/2))
+    bounding_box['x_max'] += int(np.ceil(add_w/2))
+
+    bb_height = float(bounding_box['y_max'] - bounding_box['y_min'])
+    add_h = (bb_height * scale) - bb_height
+    bounding_box['y_min'] -= int(np.floor(add_h/2))
+    bounding_box['y_max'] += int(np.ceil(add_h/2))
+
+    return bounding_box
+
+
 def stitch_image(bounding_box, slide):
 
     # img_width = int(np.ceil((bounding_box['x_max'] - bounding_box['x_min']) / TILE_SIZE) * TILE_SIZE)
@@ -129,12 +204,16 @@ def wsi(file):
     slide = OpenSlide(file)
     slide_name = file.split('/')[-1]
     regions = read_json(file + '.json')
+
     for region in regions:
         bounding_box = get_bounding_box(region)
+        if(RESIZE):
+            bounding_box = resize_bounding_box(bounding_box)
         location = (bounding_box['x_min'], bounding_box['y_min'])
         size = (bounding_box['x_max'] - bounding_box['x_min'], bounding_box['y_max'] - bounding_box['y_min'])
         image = slide.read_region(location, 0, size)
         save_image(image, region, slide_name)
+
     slide.close()
 
 

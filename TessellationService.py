8362ee08773fb684fafae2d3f6485fe18cb36e0c
SasNaw <SaschaNawrot@gmail.com>
2016-08-31 14:43:05 +0200

    restructured internal functions

--- TessellationService.py
+++ TessellationService.py
@@ -3,7 +3,7 @@
 
 # TODO delete this:
 # /home/sawn/Studium/Master/microservices/TessellationService/TessellationService.py
-# -o /home/sawn/Studium/Master/microservices/TessellationService/test /home/sawn/Studium/Master/microservices/TessellationService/img/CMU-1.svs
+# -r 200 200 -f -o /home/sawn/Studium/Master/microservices/TessellationService/test /home/sawn/Studium/Master/microservices/TessellationService/img/CMU-1.svs
 
 from openslide import OpenSlide
 from PIL import Image
@@ -20,7 +20,11 @@ global OUTPUT
 OUTPUT = None
 global FORCE
 FORCE = False
+global RESIZE
+RESIZE = None
 
+
+# ======================================   UTILITY   ======================================
 
 def read_json(path):
     try:
@@ -30,6 +34,49 @@ def read_json(path):
             return data
     except IOError:
         print('Could not load saved annotations from ' + path)
+
+
+def save_image(image, region, slide_name):
+    if(OUTPUT):
+        dest = OUTPUT + region['name']
+    else:
+        dest = region['name']
+    if not os.path.exists(dest):
+        os.makedirs(dest)
+    name = dest + '/' + slide_name + '_' + str(region['uid'])
+    if not FORCE:
+        while os.path.isfile(name):
+            name = name + "_copy"
+    if not RESIZE is None:
+        print("resize me! :D")
+    image.save(name, 'jpeg')
+    with open(name + '.context', 'w+') as file:
+        data = json.dumps(region.get('context'), ensure_ascii=False)
+        file.write(data.encode('utf-8'))
+
+
+# ======================================   DZI   ======================================
+
+
+# ======================================   WSI   ======================================
+
+def is_suppoted(file):
+    ext = (file.split('.'))[-1]
+    if(
+        'bif' in ext or
+        'mrxs' in ext or
+        'npdi' in ext or
+        'scn' in ext or
+        'svs' in ext or
+        'svslide' in ext or
+        'tif' in ext or
+        'tiff' in ext or
+        'vms' in ext or
+        'vmu' in ext
+    ):
+        return 1
+    else:
+        return 0
 
 
 def get_bounding_box(region):
@@ -71,97 +118,67 @@ def stitch_image(bounding_box, slide):
     return img_stitched
 
 
-def extract_image_from_region_dzi(slide, region):
-    width, height = slide.dimensions
-    return ''
+def dzi(file):
+    regions = read_json(file + '.json')
+    for region in regions:
+        print("dzi")
+    return None
 
 
-def extract_image_from_region_wsi(slide, region):
-    bounding_box = get_bounding_box(region)
-    location = (bounding_box['x_min'], bounding_box['y_min'])
-    size = (bounding_box['x_max'] - bounding_box['x_min'], bounding_box['y_max'] - bounding_box['y_min'])
-    return slide.read_region(location, 0, size)
-
-
-def extract_image_from_region(slide, slide_name, region, dzi):
-    if(dzi):
-        image = extract_image_from_region_dzi(slide, region)
-    else:
-        image = extract_image_from_region_wsi(slide, region)
-    if(OUTPUT):
-        dest = OUTPUT + region['name']
-    else:
-        dest = region['name']
-    if not os.path.exists(dest):
-        os.makedirs(dest)
-    name = dest + '/' + slide_name + '_' + str(region['uid'])
-    if not FORCE:
-        while os.path.isfile(name):
-            name = name + "_copy"
-    image.save(name, 'jpeg')
-    with open(name + '.context', 'w+') as file:
-        data = json.dumps(region.get('context'), ensure_ascii=False)
-        file.write(data.encode('utf-8'))
-
-
-def extract_regions(file):
+def wsi(file):
     slide = OpenSlide(file)
     slide_name = file.split('/')[-1]
     regions = read_json(file + '.json')
     for region in regions:
-        extract_image_from_region(slide, slide_name, region, 0)
+        bounding_box = get_bounding_box(region)
+        location = (bounding_box['x_min'], bounding_box['y_min'])
+        size = (bounding_box['x_max'] - bounding_box['x_min'], bounding_box['y_max'] - bounding_box['y_min'])
+        image = slide.read_region(location, 0, size)
+        save_image(image, region, slide_name)
     slide.close()
 
 
-def is_suppoted(file):
-    ext = (file.split('.'))[-1]
-    if(
-                    'bif' in ext or
-                    'mrxs' in ext or
-                    'npdi' in ext or
-                    'scn' in ext or
-                    'svs' in ext or
-                    'svslide' in ext or
-                    'tif' in ext or
-                    'tiff' in ext or
-                    'vms' in ext or
-                    'vmu' in ext
-       ):
-        return 1
-    else:
-        return 0
+# ======================================   RUN & ARGS   ======================================
 
-
-def extract_regions_from_dir(dir):
+def files_from_dir(dir):
     if not dir.endswith('/'):
         dir = dir + '/'
     contents = os.listdir(dir)
     for content in contents:
         if os.path.isdir(dir + content):
-            extract_regions_from_dir(dir + content)
+            if not content.endswith("_files"):
+                files_from_dir(dir + content)
         else:
-            if(is_suppoted(content)):
-                extract_regions(dir + content)
+            regions_from_file(dir + content)
+
+
+def regions_from_file(file):
+    if file.endswith(".dzi"):
+        dzi(file)
+    else:
+        if(is_suppoted(file)):
+            wsi(file)
 
 
 def run(input):
-    if(os.path.isfile(input)):
-        extract_regions(input)
-    elif(os.path.isdir(input)):
-        extract_regions_from_dir(input)
+    # input is folder:
+    if(os.path.isdir(input)):
+        files_from_dir(input)
+    # input is file:
+    elif(os.path.isfile(input)):
+        regions_from_file(input)
     # TODO: list
     else:
-        print("ERROR: " + input + " could not be found!")
+        print("ERROR: " + input + " not found!")
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("input", help="Either a single file, a list of files or a folder")
     parser.add_argument("-o", "--output", help="directory to store extracted images")
-    parser.add_argument("-r", "--resize", help="resize all extracted images to provided value", type=int)
+    parser.add_argument("-r", "--resize", help="resize all extracted images to provided width, height in pixel", type=int, nargs=2)
     parser.add_argument("-t", "--tessellate", help="blub", type=int)
     parser.add_argument("-f", "--force-overwrite", help="overwrite images with the same name [False]", action="store_true")
-
 
     args = parser.parse_args()
 
@@ -169,8 +186,9 @@ if __name__ == '__main__':
         print("Only one of -r, -t can be chosen at the same time")
     else:
         FORCE = args.force_overwrite
+        RESIZE = args.resize
+        OUTPUT = args.output
         if(args.output):
-            OUTPUT = args.output
             if not OUTPUT.endswith('/'):
                 OUTPUT = OUTPUT + '/'
             if not os.path.exists(OUTPUT):
